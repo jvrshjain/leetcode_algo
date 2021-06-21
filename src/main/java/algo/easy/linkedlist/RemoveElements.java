60923b73a7c602174601d53150022fd353c2c566
Varsha J <jvrshjain@gmail.com>
2021-06-21 22:04:56 +0530

    Remove element from linked list
    Signed-off-by: Varsha J <jvrshjain@gmail.com>

---
+++ src/main/java/algo/easy/linkedlist/RemoveElements.java
@@ -0,0 +1,42 @@
+package algo.easy.linkedlist;
+
+public class RemoveElements {
+
+    public ListNode removeElements(ListNode head, int elementToExclude) {
+        if(head ==null) return head;
+        System.out.printf("Remove Elements from ListNode \n");
+        ListNode ptr = head;
+        while (ptr.next != null){
+            if(ptr.next.val == elementToExclude){
+                ptr.next = ptr.next.next;
+            }else{
+                ptr = ptr.next;
+            }
+        }
+        if(head.val == elementToExclude) return head.next;
+
+        return head;
+    }
+
+    public static void main(String[] args)  {
+        //Case 1:  [1,2,6,3,4,5,6] exclude element 6 ---> result: [1,2,3,4,5]
+        ListNode head = new ListNode(1, new ListNode(2, new ListNode(6,
+                new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6)))))));
+        //Case 1:  [1,6] exclude element 6  ---> result: [1]
+//        ListNode head = new ListNode(1, new ListNode(6));
+
+        //Case 3:  [] exclude element 6  ---> result: []
+//        ListNode head = null;
+
+        //Case 4:  [6,6] exclude element 6  ---> result: []
+//        ListNode head = new ListNode(6, new ListNode(6));
+
+        RemoveElements soln = new RemoveElements();
+        ListNode resultList = soln.removeElements(head, 6);
+        while (resultList !=null ){
+            System.out.println(resultList.val);
+            resultList = resultList.next;
+        }
+
+    }
+}
